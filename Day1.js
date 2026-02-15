f1b49db793325abe3bef36d68b625226c2e6d748
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-02-16 01:08:47 +0530

    Initial Commit

---
+++ Day1.js
@@ -0,0 +1,11 @@
+//  Write a JavaScript program to:
+// 1.	Declare variables using var, let, and const.
+// 2.	Store your name, age, and whether you are a student.
+// 3.	Print them using template literals.
+
+const name ="Kanishka";
+let age = 19;
+var Is_Student = true;
+console.log(`Name : ${name}`);
+console.log(`Age : ${age}`);
+console.log(`Student : ${Is_Student}`);
