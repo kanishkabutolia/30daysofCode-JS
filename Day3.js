2b52cad3542503358c7e6ea3c34fb788f07fb6f7
Kanishka Butolia <kanishka.butolia@gmail.com>
2026-02-19 00:42:26 +0530

    Initial Commit

---
+++ Day3.js
@@ -0,0 +1,14 @@
+// Write a program to:
+// 1.	Check whether a number is positive, negative, or zero.
+// 2.	Print appropriate message.
+
+let num = -5;
+if (num > 0){
+    console.log(`The number ${num} is POSITIVE`);
+}
+else if (num === 0){
+    console.log(`The number ${num} is ZERO`);
+}
+else {
+    console.log(`The number ${num} is NEGATIVE`);
+}
